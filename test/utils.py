f2eebcd1aa2876f7ef3c937ca87c295b1f929b73
Dane <danesherbs@gmail.com>
2018-06-04 17:19:12 +0100

    Tests now failing

--- test/utils.py
+++ test/utils.py
@@ -1,24 +1,27 @@
-import pytest
 import numpy as np
 import eagcn_pytorch.utils as shang_datasets
 from datasets import lipophilicity, tox21, freesolv, hiv
 
 
 def test_lipophilicity():
-    shang_datasets.load_data("lipo", path='../data/')
-    lipophilicity.load_data()
+    theirs = shang_datasets.load_data("lipo", path='../data/')
+    ours = lipophilicity.load_data()
+    assert np.array_equal(theirs, ours)
 
 
 def test_tox21():
-    shang_datasets.load_data("tox21", path='../data/')
-    tox21.load_data()
+    theirs = shang_datasets.load_data("tox21", path='../data/')
+    ours = tox21.load_data()
+    assert np.array_equal(theirs, ours)
 
 
 def test_hiv():
-    shang_datasets.load_data("hiv", path='../data/')
-    hiv.load_data()
+    theirs = shang_datasets.load_data("hiv", path='../data/')
+    ours = hiv.load_data()
+    assert np.array_equal(theirs, ours)
 
 
 def test_freesolv():
-    shang_datasets.load_data("freesolv", path='../data/')
-    freesolv.load_data()
+    theirs = shang_datasets.load_data("freesolv", path='../data/')
+    ours = freesolv.load_data()
+    assert np.array_equal(theirs, ours)
